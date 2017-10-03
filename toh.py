f86e889bc51ecd635c0c74de307b4337002bf1a3
SIDDHARTH MUTHUKUMARAN <siddharth.muthukumaran@cloud5.com>
2017-10-03 15:35:51 -0500

    Implemented Best First Search || Sidd || 10/03/2017

--- toh.py
+++ toh.py
@@ -1,6 +1,7 @@
 from node import Node
 
-ip = [[1],[],[]]
+ip = [[1,2,3,4],[],[]]
+op = [[],[4],[1,2,3]]
 visited = []
 trace = []
 
@@ -61,8 +62,22 @@ def print_trace(trace):
         print t
 
 def find_best_move(moves,op):
-    best_so_far = []
+    all_visited = True
+    heuristic_dict = {}
     for move in moves:
+        if move not in visited:
+            all_visited = False
+            peg_sum = abs(sum(move[0]) - sum(op[0])) + abs(sum(move[1]) - sum(op[1])) + abs(sum(move[2]) - sum(op[2]))
+            heuristic_dict[peg_sum] = move
+
+    if all_visited:
+        return None
+
+    min_sum = min(heuristic_dict.keys())
+    best_move = heuristic_dict[min_sum]
+    return best_move
+
+# print find_best_move([[[2],[1,3],[4,5]],[[2],[3],[1,4,5]],[[1,2],[],[3,4,5]]],[[],[],[1,2,3,4,5]])
 
 def bestfs(n,op):
     visited.append(n.state)
@@ -76,7 +91,6 @@ def bestfs(n,op):
         else:
             bestfs(n.parent,op)
 
-    #isited.append(best)
     if best == op:
         print_trace(visited)
         print best
@@ -88,3 +102,7 @@ def bestfs(n,op):
     child.parent = n
     n.children.append(child)
     bestfs(child,op)
+
+root = Node()
+root.state = ip
+bestfs(root,op)
