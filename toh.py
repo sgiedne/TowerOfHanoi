9cecb967553231c7e74ad8fee1a2c354ddbc1d13
sharmashanil12 <sharmashanil12@gmail.com>
2017-10-03 19:05:01 -0500

    Shanil||3rdOct

--- toh.py
+++ toh.py
@@ -1,6 +1,7 @@
 from node import Node
 
-ip = [[1],[],[]]
+ip = [[1,2,3,4],[],[]]
+op = [[],[4],[1,2,3]]
 visited = []
 trace = []
 
@@ -55,3 +56,53 @@ def get_moves(state):
                 move = [state[0],[state[2][0]] + state[1],state[2][1:]]
                 moves.append(move)
     return moves
+
+def print_trace(trace):
+    for t in trace:
+        print t
+
+def find_best_move(moves,op):
+    all_visited = True
+    heuristic_dict = {}
+    for move in moves:
+        if move not in visited:
+            all_visited = False
+            peg_sum = abs(sum(move[0]) - sum(op[0])) + abs(sum(move[1]) - sum(op[1])) + abs(sum(move[2]) - sum(op[2]))
+            heuristic_dict[peg_sum] = move
+
+    if all_visited:
+        return None
+
+    min_sum = min(heuristic_dict.keys())
+    best_move = heuristic_dict[min_sum]
+    return best_move
+
+# print find_best_move([[[2],[1,3],[4,5]],[[2],[3],[1,4,5]],[[1,2],[],[3,4,5]]],[[],[],[1,2,3,4,5]])
+
+def bestfs(n,op):
+    visited.append(n.state)
+    moves = get_moves(n.state)
+    best = find_best_move(moves,op)
+
+    if not best:
+        if n.parent == None:
+            print "No solution exists"
+            exit()
+        else:
+            bestfs(n.parent,op)
+
+    if best == op:
+        print_trace(visited)
+        print best
+        print "Solution found"
+        exit()
+
+    child = Node()
+    child.state = best
+    child.parent = n
+    n.children.append(child)
+    bestfs(child,op)
+
+root = Node()
+root.state = ip
+bestfs(root,op)
